f233a6ecb763db101c540956df2c4dc25d88ed7b
coderyc <1556517393@qq.com>
2025-02-12 00:17:45 +0800

    a接口的具体实现

--- src/main/java/com/project/LoginController.java
+++ src/main/java/com/project/LoginController.java
@@ -2,7 +2,13 @@ package com.project;
 
 public class LoginController {
     // 这是a开发的a接口
-    public void login () {
+    public void login (String account, String password) {
         System.out.println("a开发的登录接口");
+        // 登录的具体实现
+        if (account == null || password == null) {
+            System.out.println("账号或密码为空");
+        } else {
+            System.out.println("登录成功");
+        }
     }
 }

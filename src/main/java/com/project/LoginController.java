d16df9324d8855f34afd6217217788e8709bb40c
coderyc <134306773+yclikestudying@users.noreply.github.com>
2025-02-12 00:15:37 +0800

    Update LoginController.java

--- src/main/java/com/project/LoginController.java
+++ src/main/java/com/project/LoginController.java
@@ -5,4 +5,9 @@ public class LoginController {
     public void login () {
         System.out.println("a开发的登录接口");
     }
+
+    // 这是b开发的b接口
+    public void register () {
+        System.out.println("b开发的注册接口")
+    }
 }
